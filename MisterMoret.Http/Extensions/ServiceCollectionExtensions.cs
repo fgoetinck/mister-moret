4d21c00652970606f080c7e1e8ff84ce69011c8d
Frédéric Goetinck-Moret <frederic.goetinck-moret@student.howest.be>
2026-05-17 12:59:57 +0200

    Add ServiceCollection extension for ApiClient registration

---
+++ MisterMoret.Http/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using MisterMoret.Http.Interfaces;
+
+namespace MisterMoret.Http.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiClient(
+        this IServiceCollection services,
+        string baseAddress)
+    {
+        services.AddHttpClient<IApiClient, ApiClient>(client =>
+        {
+            client.BaseAddress = new Uri(baseAddress);
+        });
+
+        return services;
+    }
+}

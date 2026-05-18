c2c097ea99fc489766c7800038d5e4f9f83eb4df
Frédéric Goetinck-Moret <frederic.goetinck-moret@student.howest.be>
2026-05-18 20:59:12 +0200

    Remove redundant access modifiers from interfaces

--- src/MisterMoret.Http/Authentication/IAccessTokenProvider.cs
+++ src/MisterMoret.Http/Authentication/IAccessTokenProvider.cs
@@ -3,7 +3,7 @@ namespace MisterMoret.Http.Authentication;
 public interface IAccessTokenProvider
 {
     string? GetAccessToken(string clientName);
-    public string? GetAccessToken();
+    string? GetAccessToken();
     void SetAccessToken(string clientName, string accessToken);
     void SetAccessToken(string accessToken);
 }
--- src/MisterMoret.Http/IApiClientFactory.cs
+++ src/MisterMoret.Http/IApiClientFactory.cs
@@ -3,5 +3,5 @@ namespace MisterMoret.Http;
 public interface IApiClientFactory
 {
     IApiClient CreateClient(string name);
-    public IApiClient CreateClient();
+    IApiClient CreateClient();
 }
